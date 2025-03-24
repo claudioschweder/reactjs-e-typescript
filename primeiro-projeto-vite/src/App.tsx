a0d4d984d40c552faeacf3fce2026b856550025a
Claudio Schweder <claudio.schweder@benner.com.br>
2025-03-24 20:41:05 -0300

    Termino aula 15

---
+++ primeiro-projeto-vite/src/App.tsx
@@ -0,0 +1,15 @@
+import {UserName as UName, UserEmail as UMail} from './UserName';
+
+const App = ()  =>{
+  return (
+    <div>
+      <h1>Meu primeiro componente de usuário</h1>
+      
+      <UName />
+      <UMail />
+
+    </div>    
+  )
+}
+
+export default App;
---
+++ primeiro-projeto-vite/src/UserName.tsx
@@ -0,0 +1,13 @@
+const UserName = () => {
+    return (
+        <h4>Meu nome é: Claudio Schweder</h4>
+    )
+}
+
+const UserEmail = () => {
+    return (
+        <h4>Meu email é: claudio.schweder@gmail.com</h4>
+    )
+}
+
+export {UserName, UserEmail}
